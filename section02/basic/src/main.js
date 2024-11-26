f2a3783a14ee67dd549afd2cf84d468d9bdd6b32
surina125 <jhyunsoo1125@gmail.com>
2024-11-26 22:47:18 +0900

    section02: 기본장면만들기-Renderer

---
+++ section02/basic/src/main.js
@@ -0,0 +1,11 @@
+import * as THREE from 'three';
+
+// 동적으로 캔버스 조립하기
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+const canvas = document.querySelector('#three-canvas');
+// const renderer = new Three.WebGLRenderer({ canvas: canvas });
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
