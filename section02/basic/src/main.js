2acf50c6d55c67eefa9a674d40028f69be50c90c
surina125 <jhyunsoo1125@gmail.com>
2024-11-26 23:20:51 +0900

    section02: 기본장면만들기-Camera

--- section02/basic/src/main.js
+++ section02/basic/src/main.js
@@ -9,3 +9,16 @@ const canvas = document.querySelector('#three-canvas');
 // const renderer = new Three.WebGLRenderer({ canvas: canvas });
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75, // 시야각(field of view)
+  window.innerWidth / window.innerHeight, // 종횡비(aspect)
+  0.1, // near
+  1000 // far 
+);
+camera.position.z = 5;
+scene.add(camera);
